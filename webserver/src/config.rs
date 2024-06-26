2e9df7834a7bdf6bfbd28f480f8d2e8af93d19db
Tiago Carvalho <sugoiuguu@tfwno.gf>
2024-06-26 10:04:52 +0100

    remove unused arg on webserver

--- webserver/src/config.rs
+++ webserver/src/config.rs
@@ -1,14 +1,5 @@
-#[derive(clap::ValueEnum, Clone, Debug, Copy)]
-pub enum CargoEnv {
-    Development,
-    Production,
-}
-
 #[derive(clap::Parser)]
 pub struct AppConfig {
-    #[clap(long, env, value_enum)]
-    pub cargo_env: CargoEnv,
-
     #[clap(long, env, default_value = "5000")]
     pub port: u16,
 
