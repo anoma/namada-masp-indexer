9ec377b785448a2d7f9c9b05bfa7f5ac80ff3861
Tiago Carvalho <sugoiuguu@tfwno.gf>
2024-06-27 14:03:48 +0100

    properly handle masp tx indexes

--- shared/src/transaction.rs
+++ shared/src/transaction.rs
@@ -1,9 +1,14 @@
-use std::collections::HashMap;
+use std::borrow::Cow;
 use std::fmt::Display;
 
+use namada_core::borsh::BorshDeserialize;
+use namada_core::collections::HashMap;
 use namada_core::hash::Hash;
 use namada_core::masp_primitives::transaction::Transaction as NamadaMaspTransaction;
-use namada_tx::{Section, Tx as NamadaTx};
+use namada_sdk::token::{
+    ShieldedTransfer, ShieldingTransfer, UnshieldingTransfer,
+};
+use namada_tx::{Data, Section, Tx as NamadaTx, TxCommitments};
 
 use crate::id::Id;
 use crate::tx_index::MaspTxIndex;
@@ -18,46 +23,53 @@ pub struct Transaction {
 impl Transaction {
     pub fn from_namada_tx(
         nam_tx_bytes: &[u8],
-        masp_tx_sechashes: &[Hash],
+        valid_masp_tx_sechashes: &[Hash],
     ) -> Option<Self> {
+        // TODO: handle IBC masp?
+
         let transaction = NamadaTx::try_from(nam_tx_bytes)
             .map_err(|e| e.to_string())
             .ok()?;
         let transaction_id = transaction.header_hash();
 
-        let masp_tx_sections: HashMap<_, _> = transaction
-            .sections
-            .into_iter()
-            .filter_map(|section| {
-                if !matches!(&section, Section::MaspTx(_)) {
-                    return None;
-                };
-                let sechash = section.get_hash();
-                let Section::MaspTx(masp_tx) = section else {
-                    unreachable!()
-                };
-                Some((sechash, masp_tx))
+        let all_masp_txs: HashMap<_, _> = transaction
+            .header
+            .batch
+            .iter()
+            .enumerate()
+            .filter_map(|(masp_tx_index, cmt)| {
+                let masp_tx_section_hash =
+                    get_shielded_tx_sechash(&transaction, cmt)?;
+                Some((masp_tx_section_hash, MaspTxIndex(masp_tx_index)))
             })
             .collect();
 
-        // TODO: handle IBC masp?
-        let masp_txs = masp_tx_sechashes
+        let masp_txs = valid_masp_tx_sechashes
             .iter()
-            .enumerate()
-            .filter_map(|(masp_tx_index, section_hash)| {
-                masp_tx_sections
-                    .get(section_hash)
-                    .cloned()
-                    .map(|masp_tx| (MaspTxIndex(masp_tx_index), masp_tx))
-                    .or_else(|| {
+            .filter_map(|section_hash| {
+                let Some(Cow::Borrowed(Section::MaspTx(masp_tx))) =
+                    transaction.get_section(section_hash)
+                else {
+                    tracing::warn!(
+                        %transaction_id,
+                        %section_hash,
+                        "Shielded tx not found in Namada transaction"
+                    );
+                    return None;
+                };
+
+                let masp_tx_index =
+                    all_masp_txs.get(section_hash).cloned().or_else(|| {
                         tracing::warn!(
                             %transaction_id,
                             %section_hash,
-                            %masp_tx_index,
+                            ?masp_tx,
                             "Shielded tx not found in Namada transaction"
                         );
                         None
-                    })
+                    })?;
+
+                Some((masp_tx_index, masp_tx.clone()))
             })
             .collect();
 
@@ -73,4 +85,35 @@ impl Display for Transaction {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.hash)
     }
+}
+
+fn get_shielded_tx_sechash(
+    transaction: &NamadaTx,
+    cmt: &TxCommitments,
+) -> Option<Hash> {
+    let Some(Cow::Borrowed(Section::Data(Data { data: tx_data, .. }))) =
+        transaction.get_section(&cmt.data_hash)
+    else {
+        return None;
+    };
+
+    let shielded_transfer = || {
+        let data = ShieldedTransfer::try_from_slice(tx_data).ok()?;
+        transaction.get_section(&data.section_hash)?;
+        Some(data.section_hash)
+    };
+    let unshielding_transfer = || {
+        let data = UnshieldingTransfer::try_from_slice(tx_data).ok()?;
+        transaction.get_section(&data.shielded_section_hash)?;
+        Some(data.shielded_section_hash)
+    };
+    let shielding_transfer = || {
+        let data = ShieldingTransfer::try_from_slice(tx_data).ok()?;
+        transaction.get_section(&data.shielded_section_hash)?;
+        Some(data.shielded_section_hash)
+    };
+
+    shielded_transfer()
+        .or_else(unshielding_transfer)
+        .or_else(shielding_transfer)
 }
