6cb83df0a44633277a21e694ffe0913d0aec909d
Marco Granelli <marco.granelli@ymail.com>
2024-09-19 15:04:31 +0200

    Updates `locate_masp_txs` to the single event's masp attribute

--- shared/src/block_results.rs
+++ shared/src/block_results.rs
@@ -1,10 +1,11 @@
 use std::collections::BTreeMap;
 
 use namada_sdk::events::extend::{
-    MaspTxBatchRefs, MaspTxBlockIndex, MaspTxRefs, ReadFromEventAttributes,
+    IndexedMaspData, MaspDataRefs, MaspTxRefs, ReadFromEventAttributes,
 };
 use tendermint_rpc::endpoint::block_results;
 
+// FIXME: maybe can avoid this too and use the type from namada
 pub struct IndexedMaspTxs {
     /// Mapping of block indexes to valid masp tx ids.
     pub locations: BTreeMap<usize, MaspTxRefs>,
@@ -20,18 +21,16 @@ pub fn locate_masp_txs(
             .unwrap_or(&vec![])
             .iter()
             .filter_map(|event| {
-                let index = MaspTxBlockIndex::read_from_event_attributes(
-                    &event.attributes,
-                )
-                .ok()?;
-
-                // Extract the references to the correct masp sections
-                let refs = MaspTxBatchRefs::read_from_event_attributes(
-                    &event.attributes,
-                )
-                .unwrap_or_default();
-
-                Some((index.0 as usize, refs))
+                MaspDataRefs::read_from_event_attributes(&event.attributes)
+                    .ok()
+                    .map(
+                        |IndexedMaspData {
+                             tx_index,
+                             masp_refs,
+                         }| {
+                            (tx_index.0 as usize, masp_refs)
+                        },
+                    )
             })
             .collect(),
     }
